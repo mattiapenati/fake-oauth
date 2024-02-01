f6934c46af28c9930cc29de49d081ceb8413e7ad
Mattia Penati <mattia.penati@protonmail.com>
2024-02-01 22:57:48 +0100

    database connection

--- mock-auth0/src/main.rs
+++ mock-auth0/src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+use color_eyre::eyre::Result;
+use config::Config;
+use sqlx::{sqlite::SqliteConnectOptions, ConnectOptions};
+
+mod config;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let config = Config::load()?;
+    let sqlite = SqliteConnectOptions::new()
+        .filename(&config.database)
+        .create_if_missing(true)
+        .connect()
+        .await?;
+
+    Ok(())
+}
